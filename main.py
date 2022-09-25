c472666bbcca9801cfb664c5cbbf93e69c29527f
pencilCool <yhtangcoder@gmail.com>
2022-09-25 12:42:52 +0800

    update argument message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from re import I
 from bs4 import BeautifulSoup as bs
 import ebooklib
 from ebooklib import epub
-import os 
+import os
 import argparse
 
 
@@ -13,37 +13,37 @@ def is_contains_chinese(strs):
             return True
     return False
 
+
 def get_catalog(book_path):
-  book = epub.read_epub(book_path)
-  catalog = [doc.get_content()
-              for doc in book.get_items_of_type(ebooklib.ITEM_NAVIGATION)][0]
+    book = epub.read_epub(book_path)
+    catalog = [doc.get_content()
+               for doc in book.get_items_of_type(ebooklib.ITEM_NAVIGATION)][0]
 
-  soup = bs(catalog,features="xml")
-  catalogs = [text_tag.get_text() for text_tag in soup.find_all('text')]
-  print("### {}".format(catalogs[0]))
-  for cata in catalogs[1:]: 
-    if is_contains_chinese(cata):
-      pass
-    else:
-      print(cata)
+    soup = bs(catalog, features="xml")
+    catalogs = [text_tag.get_text() for text_tag in soup.find_all('text')]
+    print("### {}".format(catalogs[0]))
+    for cata in catalogs[1:]:
+        if is_contains_chinese(cata):
+            pass
+        else:
+            print(cata)
 
-  print("\n----\n")
-
-
+    print("\n----\n")
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(
-        "import epub book and export xlsx file which contains the word list of book"
+        "import epub books under root dir and export catalogs"
     )
-    parser.add_argument("--rootDir", help="the root dir of epub books", type=str)
+    parser.add_argument(
+        "--rootDir", help="the root dir of epub books", type=str)
     args = parser.parse_args()
 
     for (root, dirs, files) in os.walk(args.rootDir, topdown=True):
-      for file in files:
-        path = os.path.join(root, file)
-        filename, file_extension = os.path.splitext(path)
-        if file_extension != ".epub":
-            continue
-        get_catalog(path)
+        for file in files:
+            path = os.path.join(root, file)
+            filename, file_extension = os.path.splitext(path)
+            if file_extension != ".epub":
+                continue
+            get_catalog(path)
